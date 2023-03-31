ea29974bb241034d81d7a57bb595a2efaf23aa9a
gustavormz <gustavormzrmz@gmail.com>
2023-03-30 23:56:00 -0600

    feat: filter rows by Nombres, DNI and Telefono

--- src/components/inputs/base/InputBase.jsx
+++ src/components/inputs/base/InputBase.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import './styles.css'
 
 const InputBase = ({
-  title = '',
-  placeholder = ''
+  title,
+  placeholder,
+  value,
+  handleOnChange
 }) => {
   return (
-    <input className='input-base-root' title={title} placeholder={placeholder}  />
+    <input
+      onChange={handleOnChange}
+      value={value}
+      className='input-base-root'
+      title={title}
+      placeholder={placeholder}  />
   )
+}
+
+InputBase.propTypes = {
+  title: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  handleOnChange: PropTypes.func,
+}
+
+InputBase.defaultProps = {
+  title: '',
+  placeholder: '',
+  value: '',
 }
 
 export default InputBase
--- src/components/inputs/dropdownmenu/DropDownMenu.jsx
+++ src/components/inputs/dropdownmenu/DropDownMenu.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import './styles.css'
 import './function.js'
 
 const DropDownMenu = ({
-    items=[]
+  items,
+  value,
+  handleOnChange,
 }) => {
-    return(
-        <div className="sel">
-            <select className="dropdownselect">
-                {items.map(function(element){
-                    return (<option value={element}>{element}</option>);
-                })}                
-            </select>
-        </div>
-        
-    )
+  return(
+    <div className="sel">
+      <select value={value} onChange={handleOnChange} className="dropdownselect">
+        {items.map(function(element){
+          return (<option key={element} value={element}>{element}</option>);
+        })}                
+      </select>
+    </div>  
+  )
+}
+
+DropDownMenu.propTypes = {
+  items: PropTypes.array,
+  value: PropTypes.string,
+  handleOnChange: PropTypes.func,
+}
+
+DropDownMenu.defaultProps = {
+  items: [],
 }
 
 export default DropDownMenu
-{/* return '<option value='+element+'>'+element+'</option>'; */}
